1273b379ff55e1a93cb7eefe878270bc69f30134
Stablest <marlonlocci@hotmail.com>
2023-03-01 06:14:52 -0300

    deploy

--- main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Home from './components/templates/home'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-)
